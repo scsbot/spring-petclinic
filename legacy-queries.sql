4e9e9055a9fab9478b9b274376b1f25ed34ad933
Aleksandar Savov <aleksandar.savov@broadcom.com>
2026-09-16 16:28:51 +0300

    PG migration

--- legacy-queries.sql
+++ legacy-queries.sql
@@ -1,7 +1,13 @@
 -- canary: unrelated line, must survive untouched
-SELECT * FROM events WHERE created_at > NOW() - INTERVAL '2 ago days';
+SELECT * FROM events WHERE created_at > NOW() - INTERVAL '2 days ago';
+-- TODO [PG17.10-adminpack]: adminpack removed - drop this statement
 CREATE EXTENSION adminpack;
+-- TODO [PG17.10-blkread]: renamed to shared_blk_read_time in pg_stat_statements only - pg_stat_database keeps blk_read_time unchanged, check which view this queries
+-- TODO [PG17.10-blkwrite]: renamed to shared_blk_write_time in pg_stat_statements only - pg_stat_database keeps blk_write_time unchanged, check which view this queries
 SELECT blk_read_time, blk_write_time FROM pg_stat_database;
+-- TODO [PG17.10-buffersbackend]: buffers_backend/buffers_backend_fsync removed - use pg_stat_io or pg_stat_checkpointer
 SELECT buffers_backend, buffers_backend_fsync FROM pg_stat_bgwriter;
+-- TODO [PG17.10-maxdeadtuples]: renamed to max_dead_tuple_bytes AND changed from a tuple count to a byte figure - do not rename in place, the value's units changed
+-- TODO [PG17.10-numdeadtuples]: renamed to num_dead_item_ids - PG17 changed how dead tuples are counted (indexed item IDs, not tuples) - do not rename in place, the value's semantics changed
 UPDATE pg_stat_progress_vacuum SET max_dead_tuples = 100, num_dead_tuples = 5;
-SELECT colliculocale, daticulocale FROM pg_collation;
+SELECT colllocale, datlocale FROM pg_collation;
--- src/main/java/com/example/legacy/LegacyReplicationSnippets.java
+++ src/main/java/com/example/legacy/LegacyReplicationSnippets.java
@@ -1,5 +1,6 @@
 package com.example.legacy;
 
+// TODO [pgjdbc]: logical replication is broken from 42.7.11 (open issue #4328) - a routine idle timeout kills the stream. Pin 42.7.10 until it is fixed.
 import org.postgresql.replication.PGReplicationStream;
 
 class LegacyReplicationSnippets {
@@ -9,9 +10,11 @@ class LegacyReplicationSnippets {
 	}
 
 	void reportStats(java.sql.ResultSet rs) throws java.sql.SQLException {
+		// TODO [PG17.10-blkread]: renamed to shared_blk_read_time in pg_stat_statements only - pg_stat_database keeps blk_read_time unchanged, check which view this queries
 		long blkReadTime = rs.getLong("blk_read_time");
+		// TODO [PG17.10-blkwrite]: renamed to shared_blk_write_time in pg_stat_statements only - pg_stat_database keeps blk_write_time unchanged, check which view this queries
 		long blkWriteTime = rs.getLong("blk_write_time");
-		String query = "SELECT * FROM events WHERE created_at > NOW() - INTERVAL '2 ago days'";
+		String query = "SELECT * FROM events WHERE created_at > NOW() - INTERVAL '2 days ago'";
 	}
 
 }
